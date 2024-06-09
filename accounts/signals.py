149b35825cc6d4854b94a91db5070c23932df419
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-06-09 15:35:11 +0200

    signals.py aktualisieren

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,7 +1,6 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+from django.db.models.signals import post_save 
 from django.dispatch import receiver
-from .models import UserProfile
+from .models import User, UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
